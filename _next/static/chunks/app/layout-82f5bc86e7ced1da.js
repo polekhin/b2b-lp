ba1b882e50aa323a5843cd6a62ea77faf1f46f19
Aleksey Polekhin <polekhin@users.noreply.github.com>
2026-08-14 02:33:23 +0300

    Update landing page deployment

---
+++ _next/static/chunks/app/layout-82f5bc86e7ced1da.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[177],{1602:()=>{},2936:()=>{},4501:(e,s,n)=>{Promise.resolve().then(n.t.bind(n,2936,23)),Promise.resolve().then(n.t.bind(n,1602,23))}},e=>{e.O(0,[435,937,770,358],()=>e(e.s=4501)),_N_E=e.O()}]);
---
+++ _next/static/chunks/next/dist/client/components/builtin/global-error-c8149e1e20f85e49.js
@@ -0,0 +1,1 @@
+(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[337],{8315:(e,s,_)=>{Promise.resolve().then(_.t.bind(_,4043,23))}},e=>{e.O(0,[937,770,358],()=>e(e.s=8315)),_N_E=e.O()}]);
